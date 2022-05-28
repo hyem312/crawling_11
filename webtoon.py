007cf001c67de7b42c99405c4c0d7ff7d3139059
hyem312 <101248828+hyem312@users.noreply.github.com>
2022-05-28 23:41:32 +0900

    Add files via upload

--- webtoon.py
+++ webtoon.py
@@ -1,20 +1,42 @@
-import requests                                                      
-from bs4 import BeautifulSoup   
-                                
-url="https://comic.naver.com/webtoon/weekday.nhn"                        
-res=requests.get(url)
-res.raise_for_status()
-soup=BeautifulSoup(res.text,"lxml")
+import requests
+from bs4 import BeautifulSoup, BeautifulStoneSoup
+import csv
+import time 
 
-tab = soup.find('div',{'class':'col_selected'})                    
-webtoons = tab.find_all('a',{'class':'title'})                       
+url_1 = 'https://comic.naver.com/index'
+keyword = input('')
+url_2 = 'https://comic.naver.com/search?keyword='+keyword
 
-name=[]
-for webtoon in webtoons:
-    title=webtoon.get_text()
-    print(title)
-    name.append(title)
+res_1 = requests.get(url_2)
+res_1.raise_for_status()
+soup = BeautifulSoup(res_1.text, "lxml")
+
+webtoon = soup.find("h5")
+link = webtoon.a["href"]
+url_3 = 'https://comic.naver.com'+link
+
+res_2 = requests.get(url_3)
+res_2.raise_for_status()
+soup_2 = BeautifulSoup(res_2.text, "lxml")
+
+titles = soup_2.find_all("td", attrs={"class":"title"})
+stars = soup_2.find_all("div", attrs={"class":"rating_type"})
+
+num_1 = []
+num_2 = []
+file = []
+for title in titles:
+    name = title.a.get_text()
+    num_1.append(name)
+    
+for star in stars:
+    score = star.strong.get_text()
+    num_2.append(score)
+
+for i in range(10):
+    print(f"제목: {num_1[i]} ,평점: {num_2[i]}")
+    file.append(f"제목: {num_1[i]} ,평점: {num_2[i]}")
 
 import pandas as pd
-data=pd.DataFrame(name)
-data.to_csv('./data.csv')
+data=pd.DataFrame(file)
+data.to_csv('./webtoons.csv')
